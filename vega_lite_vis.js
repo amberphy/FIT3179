6dd164c702a81f7da7a60d13a01c8e9d92ae6658
Amber Pang <126540384+amberphy@users.noreply.github.com>
2024-09-21 00:03:24 +0800

    Homework Week 9

---
+++ vega_lite_vis.js
@@ -0,0 +1,3 @@
+var vg_1 = "tourism_expenditures.vg.json"; vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
